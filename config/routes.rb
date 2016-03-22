95073c78827514ffe13d6859afff517ffa3351f6
Karine Valença <valenca.karine@gmail.com>
2016-03-22 14:42:36 -0300

    update create user with email and password but some fields doesn't go to db

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   resources :projects
   resources :users
-  resources :users
+  #resources :users
 
   get 'projects/new'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -59,5 +59,5 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-  root :to => "home#index"
+  #root :to => "home#index"
 end
