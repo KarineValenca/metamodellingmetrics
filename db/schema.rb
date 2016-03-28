265a5af5c61fd2260b979adf3c180a07710abafb
Karine Valença <valenca.karine@gmail.com>
2016-03-28 18:41:05 -0300

    fix user attributes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160322152758) do
+ActiveRecord::Schema.define(version: 20160328213254) do
 
   create_table "projects", force: :cascade do |t|
     t.string   "name_project",       limit: 255
@@ -30,7 +30,6 @@ ActiveRecord::Schema.define(version: 20160322152758) do
   create_table "users", force: :cascade do |t|
     t.string   "first_name",             limit: 255
     t.string   "last_name",              limit: 255
-    t.string   "company",                limit: 255
     t.string   "username",               limit: 255
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
