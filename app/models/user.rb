a30f02fea5855d89171fb0c73d5e451a5d963fc6
Karine Valença <valenca.karine@gmail.com>
2016-05-11 19:57:23 -0300

    listing project by users

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,5 @@ class User < ActiveRecord::Base
  # devise :database_authenticatable, :registerable,
   #       :recoverable, :rememberable, :trackable, :validatable
 	has_and_belongs_to_many :projects
+	belongs_to :company
 end
